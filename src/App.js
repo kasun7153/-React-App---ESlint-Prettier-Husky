f0173eddc7aeea05d5550e0dc2d78fe2e859c66e
kasun7153 <kasun7153@gmail.com>
2021-06-21 23:58:01 +0530

    Eslint, Prettier, Husky React Begin Project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  const name = 'kasun';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Eslint, Prettier included</p>
+        <p>Eslint, Prettier, Husky React Begin Project</p>
       </header>
     </div>
   );
